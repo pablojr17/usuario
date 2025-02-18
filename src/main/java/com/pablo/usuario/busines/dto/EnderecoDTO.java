9b03f9e51d047626c8c02429349bf9d81e3e66b1
pablojr17 <pablojr17@gmail.com>
2025-02-17 22:56:43 -0300

    Adicionado o metodo que salva dados de usuario

---
+++ src/main/java/com/pablo/usuario/busines/dto/EnderecoDTO.java
@@ -0,0 +1,17 @@
+package com.pablo.usuario.busines.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EnderecoDTO {
+    private String rua;
+    private Long numero;
+    private String complemento;
+    private String cidade;
+    private String estado;
+    private String cep;
+}
