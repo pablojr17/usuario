1cec5c6615bdbd50b0265ee8bfb7dad29c1b310e
pablojr17 <pablojr17@gmail.com>
2025-02-22 14:48:34 -0300

    Adicionado o metodo get, delete e login

---
+++ src/main/java/com/pablo/usuario/infrastructure/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.pablo.aprendendospring.infrastructure.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String mensagem){
+        super(mensagem);
+    }
+
+    public ResourceNotFoundException(String mensagem, Throwable throwable){
+        super(mensagem, throwable);
+    }
+}
